4b5b0634ab0844d5270e9e7de29ce00694befd03
samarthhatte <109466822+samarthhatte@users.noreply.github.com>
2026-02-01 12:38:59 +0530

    Add "Forgot Password?" feature with email reset functionality in LoginActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agpitcodeclub.codeclubagpit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.4"
+        versionCode = 16
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
