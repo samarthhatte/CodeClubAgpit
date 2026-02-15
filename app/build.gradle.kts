f5c497807f82c8507ceda1e100ff62478bc9670d
samarthhatte <109466822+samarthhatte@users.noreply.github.com>
2026-02-15 09:47:50 +0530

    Enhance ChatActivity and member item layout with clickable profile images; update versionCode to 33 and versionName to 4.3 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.agpitcodeclub.codeclubagpit"
         minSdk = 24
         targetSdk = 35
-        versionCode = 31
-        versionName = "4.1"
+        versionCode = 33
+        versionName = "4.3"
         ndkVersion = "28.0.12433510"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
